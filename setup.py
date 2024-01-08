e7c9a95a74bc7eeebfbbc767ae7bb363d9603ba9
Israel Agundis <israel.agundis@affirm.com>
2024-01-08 11:36:55 -0500

    Update version label

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ except ImportError:
     from distutils.core import setup
 
 NAME = "feast"
-VERSION = "0.28+affirm185"
+VERSION = "0.29+affirm1"
 DESCRIPTION = "Python SDK for Feast @ Affirm"
 URL = "https://github.com/feast-dev/feast"
 AUTHOR = "Feast"
